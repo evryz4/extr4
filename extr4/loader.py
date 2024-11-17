31964f5bddf26527c9132ca5933cf49fe69c8203
evryz4 <125790562+evryz4@users.noreply.github.com>
2024-11-17 14:54:50 +0300

    moved Config class to utils.py

--- extr4/loader.py
+++ extr4/loader.py
@@ -2,28 +2,6 @@ import os
 import json
 import importlib.machinery
 
-configfile = 'config.json'
-
-class Config:
-    '''Class for working with config'''
-
-    def __init__(self, configfile='config.json'):
-        self.file = configfile
-        with open(configfile) as file:
-            self.config = json.loads(file.read())
-
-    def _save(self):
-        with open(configfile, 'w') as file:
-            file.write(json.dumps(self.config, indent=4))
-    
-    def edit(self, **vars):
-        for var in vars:
-            self.config[var] = vars[var]
-        self._save()
-    
-    def get(self):
-        return self.config
-
 def strings(*, name: str = 'Module', version: str = '1.0'):
     return {
         'name': name,
